724758141f251323a86d8d106d554f32e06c3031
Ben Sparks <bensparks0@gmail.com>
2021-06-26 15:23:41 +0200

    Added mint, burn, onlyController to token

--- contracts/USDZ.sol
+++ contracts/USDZ.sol
@@ -6,7 +6,24 @@ contract USDZ is ERC20 {
     // contract with permission to mint/burn tokens
     address public controller;
 
-    constructor(address _controller, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    constructor(
+        address _controller,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
         controller = _controller;
+    }
+
+    function mint(address _account, uint256 _amount) external onlyController() {
+        _mint(_account, _amount);
+    }
+
+    function burn(address _account, uint256 _amount) external onlyController() {
+        _burn(_account, _amount);
+    }
+
+    modifier onlyController() {
+        require(msg.sender == controller, "only controller has permission");
+        _;
     }
 }
