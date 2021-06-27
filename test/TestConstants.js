dd1858e8eec40e7de8dd780d35232dc0e7f1f26f
Ben Sparks <bensparks0@gmail.com>
2021-06-27 14:08:09 +0200

    Added test constants for wallets and contract addresses

---
+++ test/TestConstants.js
@@ -0,0 +1,18 @@
+
+export const CONSTANTS = {
+    CONTRACTS: {
+        SUSHI: {
+            // https://etherscan.io/address/0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F#code
+            ROUTER: "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+            // https://etherscan.io/address/0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac#code
+            FACTORY: "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac",
+            // https://etherscan.io/address/0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272#code
+            XSUSHI: "0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272"
+        },
+    },
+    WALLETS: {
+        // https://etherscan.io/address/0xf977814e90da44bfa03b6295a0616a897441acec
+        // Binance 8 Wallet
+        XSUSHI_WHALE: "0xf977814e90da44bfa03b6295a0616a897441acec",
+    }
+}
