472d4d9cf0f60940197f91e430eba820207def30
Ben Sparks <bensparks0@gmail.com>
2021-08-03 15:57:50 +0200

    Added comments to swap path assets

--- test/TestConstants.js
+++ test/TestConstants.js
@@ -20,11 +20,11 @@ const CONSTANTS = {
             liqFeeSender: 200,
             interestRate: 300,
             borrowThreshold: 20000,         // 200%
-            liqThreshold: 15000,    // 150%
+            liqThreshold: 15000,            // 150%
             xSushiToUsdcPath: [
-                "0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272",
-                "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-                "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+                "0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272",   // xSUSHI
+                "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",   // WETH
+                "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"    // USDC
             ]
         },
         USDZ: {
